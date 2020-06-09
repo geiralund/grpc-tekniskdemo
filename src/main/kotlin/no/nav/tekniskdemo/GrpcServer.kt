b8ec17b494cbdf53087e5607c40433a6339a5ae0
Geir André Lund <geir.andre.lund@nav.no>
2020-06-09 10:01:06 +0200

    Kotling version of person service

--- src/main/kotlin/no/nav/tekniskdemo/GrpcServer.kt
+++ src/main/kotlin/no/nav/tekniskdemo/GrpcServer.kt
@@ -1,21 +1,27 @@
 package no.nav.tekniskdemo
 
+import de.huxhorn.sulky.ulid.ULID
 import io.grpc.Server
 import io.grpc.ServerBuilder
+import io.grpc.Status
+import io.grpc.StatusException
 import io.grpc.stub.StreamObserver
 import no.nav.tekniskdemo.chat.Chat
 import no.nav.tekniskdemo.chat.Chat.ChatMessageFromServer
 import no.nav.tekniskdemo.chat.ChatServiceGrpc
-import java.util.*
+import no.nav.tekniskdemo.person.Person
+import no.nav.tekniskdemo.person.PersonId
+import no.nav.tekniskdemo.person.PersonRequest
+import no.nav.tekniskdemo.person.PersonServiceGrpcKt
 
-
-internal class ChatGrpcServer(
+internal class GrpcServer(
     port: Int
 ) {
 
     private val server: Server = ServerBuilder
         .forPort(port)
         .addService(ChatService())
+        .addService(PersonService())
         .build()
 
 
@@ -32,6 +38,29 @@ internal class ChatGrpcServer(
 
     fun stop() {
         server.shutdown()
+    }
+
+    internal class PersonService : PersonServiceGrpcKt.PersonServiceCoroutineImplBase() {
+
+        private val ulid = ULID()
+        private val personStore = mutableMapOf<String, Person>()
+
+        override suspend fun createPerson(request: PersonRequest): Person {
+            return personStore.computeIfAbsent(ulid.nextULID()) { personId ->
+                Person.newBuilder().apply {
+                    name = request.name
+                    id = PersonId.newBuilder().apply {
+                        id = personId
+                    }.build()
+                }.build()
+            }
+        }
+
+        override suspend fun getPerson(request: PersonId): Person {
+            return personStore[request.id]
+                ?: throw StatusException(Status.NOT_FOUND.withDescription("Person with id ${request.id} not found"))
+        }
+
     }
 
     internal class ChatService : ChatServiceGrpc.ChatServiceImplBase() {
@@ -54,7 +83,7 @@ internal class ChatGrpcServer(
                 }
 
                 override fun onError(t: Throwable?) {
-                    // todo : do something!
+                    // todo : something!
                 }
 
                 override fun onCompleted() {
@@ -69,6 +98,6 @@ internal class ChatGrpcServer(
 }
 
 fun main() {
-    val chatServer = ChatGrpcServer(50051)
-    chatServer.start()
+    val server = GrpcServer(50051)
+    server.start()
 }
